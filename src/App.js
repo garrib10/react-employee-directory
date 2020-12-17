48f2645962d176ed8c55bd8a252043d44983f43c
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-17 15:24:01 -0500

    updated NavBar syntax on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
-import Nav from './components/NavBar'
+import NavBar from './components/NavBar';
 import Main from './components/Main'
 import Footer from './components/Footer'
-import NavBar from './components/NavBar';
+
 
 function App() {
   return (
