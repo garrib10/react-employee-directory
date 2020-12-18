ad87845cd94e78aba2a76bdcb3101e0ba688e03d
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-17 19:45:38 -0500

    Wrapper Component works

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Wrapper from './components/Wrapper';
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Wrapper>
       <Main />
+      </Wrapper>
       <Footer />
+     
     </div>
   );
 }
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./wrapper.css"; 
+
+function Wrapper (props){
+   return <main className="wrapper">{props.children}</main>;
+}
+
+export default Wrapper;
+
