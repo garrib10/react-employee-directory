638b1777c3c3a08c9e6cbfd64150c9358eac1545
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-21 22:12:51 -0500

    redeploy

--- src/utils/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    getUsers: function() {
-        return axios.get("https://randomUser.me/api/?results=200&nat=us")
-    }
-}
